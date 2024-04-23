51f857a92f82fd006cc6ac8f77d1f0b00aa26299
Zitronenjoghurt <gersdorfdominik@gmail.com>
2024-04-23 19:28:33 +0200

    rework home screen a bit for testing

--- src/screens/home.rs
+++ src/screens/home.rs
@@ -1,12 +1,21 @@
 use crate::screens::screen_manager::Screen;
 
-pub struct HomeScreen {
-    pub value: u32,
-}
+pub struct HomeScreen {}
 
 impl Screen for HomeScreen {
     fn update(&mut self, ui: &mut egui::Ui) {
-        ui.label("Home Screen");
-        ui.add(egui::Slider::new(&mut self.value, 1..=120).text("Slider"));
+        ui.vertical_centered(|ui| {
+            ui.add_space(50.0);
+
+            ui.heading("LemCom Messenger");
+            ui.add_space(10.0);
+
+            if ui.button("Button 1").clicked() {
+                println!("Button 1 clicked");
+            }
+            if ui.button("Button 2").clicked() {
+                println!("Button 2 clicked");
+            }
+        });
     }
 }
